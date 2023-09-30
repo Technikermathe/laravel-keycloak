d64cc9fe415580494f38bc3e6912aaad85547c6b
Pascale Beier <mail@pascalebeier.de>
2023-09-30 14:39:59 +0200

    fix: Add Auth Guard

--- src/KeycloakServiceProvider.php
+++ src/KeycloakServiceProvider.php
@@ -4,11 +4,13 @@ namespace Technikermathe\Keycloak;
 
 use GuzzleHttp\Psr7\HttpFactory;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 use Psr\Http\Message\UriFactoryInterface;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Technikermathe\Keycloak\Auth\Guard\KeycloakGuard;
 
 class KeycloakServiceProvider extends PackageServiceProvider
 {
@@ -47,6 +49,11 @@ class KeycloakServiceProvider extends PackageServiceProvider
                 http: Http::baseUrl($baseUrl)->throw(),
                 baseUrl: $baseUrl->__toString()
             );
+        });
+
+        Auth::extend('keycloak', function ($app, $name, array $config) {
+            $provider = Auth::createUserProvider($config['provider']);
+            return new KeycloakGuard($provider, $app->request);
         });
 
         return $this;

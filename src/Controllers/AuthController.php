a4cae833b8d4408e71926ea45b5ef48a5a52a25d
PascaleBeier <PascaleBeier@users.noreply.github.com>
2023-12-20 14:54:54 +0000

    Fix styling

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -55,7 +55,7 @@ class AuthController extends Controller
         // Change code for token
         $token = Keycloak::getToken();
 
-        if (!Auth::validate($token->toArray())) {
+        if (! Auth::validate($token->toArray())) {
             Keycloak::forgetState();
             Keycloak::forgetToken();
 
